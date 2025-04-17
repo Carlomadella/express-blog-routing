ebb8f4739fba1281c978787afa08bcf29a2211aa
Carlomadella <madella871@gmail.com>
2025-04-17 16:54:35 +0200

    [UPD] importato il router nel file index

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+
+// importo il router dei post
+const pizzasRouter = require('./routers/posts.js');
+
+
 const port = 3000
 
 app.get('/', (req, res) => {
