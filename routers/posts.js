efc0dbba6a44f187421050acd8847b44e5c895ce
Carlomadella <madella871@gmail.com>
2025-04-17 16:44:43 +0200

    [UPD] creata operazione per visualizzare un post specifico

--- routers/posts.js
+++ routers/posts.js
@@ -6,3 +6,12 @@ const posts = express.Router();
 posts.get('/', (req, res) => {
     res.send("Lista dei post");
 });
+
+// UTILIZZO I METODI HTTP PER AZIONI SPECIFICHE SULLE RISORSE
+
+// Read: Visualizzo un elemento con id specifico (Show)
+posts.get("/:id", (req, res) => {
+    req.params.id;
+    const id = req.params.id;
+    res.send(`Visualizzo il post con id ${id}`);
+});
